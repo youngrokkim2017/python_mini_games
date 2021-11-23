091c3b8f9d0ef627b2a35deb578ddfe73fc4227c
Patrick Kim <patrick.yrkim@gmail.com>
2021-11-23 01:04:23 -0800

    Update tic tac toe with smart computer player

--- player.py
+++ player.py
@@ -37,3 +37,65 @@ class HumanPlayer(Player):
             except ValueError:
                 print('Invalid swuare. Try again')
         return val
+
+# minimax
+class GeniusComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+    
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            square = random.choice(game.available_moves())
+        else: 
+            square = self.minimax(game, self.letter)['position']
+        return square
+
+    def minimax(self, state, player):
+        max_player = self.letter    #yourself
+        other_player = 'O' if player == 'X' else 'X'    # the other player
+
+        #  check if previous move is winner
+        # recursion base case
+        if state.current_winner == other_player:
+            return {
+                'position': None,
+                'score': 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (state.num_empty_squares() + 1)
+            }
+        elif not state.empty_squares():     # no empty squares
+            return {
+                'position': None,
+                'score': 0
+            }
+        
+        if player == max_player:
+            best = {
+                'position': None,
+                'score': -math.inf
+            }
+        else:
+            best = {
+                'position': None,
+                'score': math.inf
+            }
+
+        for possible_move in state.available_moves():
+            # step 1: make a move, try that spot
+            state.make_move(possible_move, player)
+
+            # step 2: recurse using minimax to simulate a game after that move
+            simulated_score = self.minimax(state, other_player)
+
+            # step 3: undo move
+            state.board[possible_move] = ' '
+            state.current_winner = None
+            simulated_score['position'] = possible_move
+
+            # step 4: update the dictionaries if necessary
+            if player == max_player:
+                if simulated_score['score'] > best['score']:
+                    best = simulated_score
+            else:
+                if simulated_score['score'] < best['score']:
+                    best = simulated_score
+            
+        return best
--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,5 +1,5 @@
 import time
-from player import HumanPlayer, RandomComputerPlayer
+from player import HumanPlayer, RandomComputerPlayer, GeniusComputerPlayer
 
 class TicTacToe:
     def __init__(self):
@@ -103,6 +103,7 @@ def play(game, x_player, o_player, print_game=True):
 
 if __name__ == '__main__':
     x_player = HumanPlayer('X')
-    o_player = RandomComputerPlayer('O')
+    # o_player = RandomComputerPlayer('O')
+    o_player = GeniusComputerPlayer('O')
     ttt = TicTacToe()
     play(ttt, x_player, o_player, print_game=True)
